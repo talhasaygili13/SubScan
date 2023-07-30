df85546f39d6bcc401932b9483df57f65540292d
talhasaygili <talhasaygili67@gmail.com>
2023-07-30 22:22:45 +0300

    SubScan

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+print('SubScan ')
+target_input = input('Enter your target website:\n')
+
+def make_request(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+with open('subdomainlist.txt', 'r') as subdomain_list:
+    for word in subdomain_list:
+        word = word.strip()
+        url ='http://' + word + '.' + target_input
+        response = make_request(url)
+        if response:
+            print('Found subdomain -----> ' + url)
